d1d7440caa4e1284b914abf9c6d037b2ffc454c2
Iamneo <techadmin@iamneo.ai>
2023-09-26 11:12:16 +0000

    Backup Commit

--- ComplaintManagementSystem/Controllers/ComplaintController.cs
+++ ComplaintManagementSystem/Controllers/ComplaintController.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+namespace dotnetapp.Controllers
+{
+    public class ComplaintController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+          public BatchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    public IActionResult AvailableBatches()
+    {
+        var data=_context.Batches.ToList();
+        return View(data);
+    }
+       public IActionResult BookedBatches()
+        {
+            return View();
+        }
+    }
+}
