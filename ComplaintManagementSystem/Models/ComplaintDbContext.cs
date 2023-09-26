63b6bfd4bcbb9ae5882a9eacc831ad6b726515e0
Iamneo <techadmin@iamneo.ai>
2023-09-26 10:59:43 +0000

    Backup Commit

--- ComplaintManagementSystem/Models/ComplaintDbContext.cs
+++ ComplaintManagementSystem/Models/ComplaintDbContext.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using dotnetapp.Models;
+using System;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+
+public class ApplicationDbContext : DbContext
+    {
+public ApplicationDbContext()
+        {
+        }  
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Complaint> Complaints { get; set; }
+        public virtual DbSet<CustomerServiceRepresentive> CustomerServiceRepresentive { get; set; }
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("User ID=sa;password=examlyMssql@123;server=localhost;Database=FrenchTutionDB;trusted_connection=false;Persist Security Info=False;Encrypt=False;");
+            }
+        }
+        // Write your ApplicationDbContext here...
+    }
