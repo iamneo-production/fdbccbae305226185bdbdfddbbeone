287a6ecfab0a08ff987abc2387d0efff407d0fba
Iamneo <techadmin@iamneo.ai>
2023-09-26 10:53:55 +0000

    Backup Commit

--- ComplaintManagementSystem/Controllers/CustomerServiceRepresentativeController.cs
+++ ComplaintManagementSystem/Controllers/CustomerServiceRepresentativeController.cs
@@ -1,10 +1,1 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace dotnetapp.Models{
-public class CustomerServiceRepresentive{
-    public int RepresentativeID {get;set;}
-    public string FirstName {get;set;}
-    public string LastName {get;set;}
-     
-    public string Email {get;set;}
-     
-}}
+﻿
--- ComplaintManagementSystem/Models/CustomerServiceRepresentative.cs
+++ ComplaintManagementSystem/Models/CustomerServiceRepresentative.cs
@@ -1,1 +1,11 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+namespace dotnetapp.Models{
+public class CustomerServiceRepresentive{
+    public int RepresentativeID {get;set;}
+    public string FirstName {get;set;}
+    public string LastName {get;set;}
+     
+    public string Email {get;set;}
+     
+}}
