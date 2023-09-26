4c743a424e2dae1da4417b33c2efb90c6325599e
Iamneo <techadmin@iamneo.ai>
2023-09-26 10:23:03 +0000

    Backup Commit

--- ComplaintManagementSystem/Controllers/CustomerServiceRepresentativeController.cs
+++ ComplaintManagementSystem/Controllers/CustomerServiceRepresentativeController.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+namespace dotnetapp.Models{
+public class Batch{
+    public int BatchID {get;set;}
+    public DateTime StartTime {get;set;}
+    public DateTime EndTime {get;set;}
+    [Display(Name="Remaining")]
+    public int Capacity {get;set;}
+    public ICollection<Student> Students {get;set;}
+}}
